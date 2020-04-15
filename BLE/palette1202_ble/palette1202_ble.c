880f34686dd672ca65d56487144df14c7cdd9ef6
niltea <futurabodoni@gmail.com>
2020-04-15 20:12:45 +0900

    moved BLE firmware src directory

--- BLE/palette1202_ble/palette1202_ble.c
+++ BLE/palette1202_ble/palette1202_ble.c
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "palette1202.h"
+#include "palette1202_ble.h"
 #include <string.h>
 
 void keyboard_post_init_user()
